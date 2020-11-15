1360a27d4e259fed708339557f9ed8fa3d616fb3
okorkov <alex.okorkov@gmail.com>
2020-11-15 13:29:43 -0800

    commit tree 9d70777c5bc40599687b6306fd140074d43b7233

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    last_char =  self[self.length - 1]
+    last_char == "."
   end
 
   def question?
-
+    last_char =  self[self.length - 1]
+    last_char == "?"
   end
 
   def exclamation?
-
+    last_char =  self[self.length - 1]
+    last_char == "!"
   end
 
   def count_sentences
-
+    string = self
+    array_split = string.split(/[!?.]/)
+    array_split.map {|element| array_split.delete(element) if element.length < 2}
+    return array_split.length 
   end
 end
